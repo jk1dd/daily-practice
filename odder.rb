3d39b70c5df627d155978d4814157bb310aecd04
jk1dd <portajonk@gmail.com>
2017-09-03 15:23:16 -0400

    add oddone exercise, unfinished

---
+++ odder.rb
@@ -0,0 +1,24 @@
+# Create a method that takes an array as an input, and outputs the index at which the sole odd number is located.
+#
+# This method should work with arrays with negative numbers. If there is no negative number in the array, then the method should output -1.
+#
+# Examples:
+#
+# oddOne([2,4,6,7,10]) # => 3
+# oddOne([2,16,98,10,13,78]) # => 4
+# oddOne([4,-8,98,-12,-7,90,100]) # => 4
+# oddOne([2,4,6,8]) # => -1
+
+
+def oddOne(arr)
+
+  arr.each_with_index do |n, i|
+    relevant_index = 0
+    if n.odd?
+      relevant_index = i
+    end
+  end
+  if relevant_index == nil
+    -1
+  end
+end

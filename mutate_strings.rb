7abd4dc4f04b5f23d55e56a30af9968eb0d029a2
jk1dd <portajonk@gmail.com>
2017-09-18 15:06:32 -0400

    create printer error

--- mutate_strings.rb
+++ mutate_strings.rb
@@ -15,14 +15,14 @@
 
 # store (puts?) the intermediate strings
 
-def mutate_my_strings(s1,s2)
-  puts s1
-  s1.chars.each_with_index do |n, i|
-    s1[i] = s2[i]
-    puts s1
-  end
-  # all
-end
+# def mutate_my_strings(s1,s2)
+#   puts s1
+#   s1.chars.each_with_index do |n, i|
+#     s1[i] = s2[i]
+#     puts s1
+#   end
+#   # all
+# end
 
 # def mutate_my_strings(s1,s2)
 #   all = s1
@@ -32,9 +32,17 @@ end
 #   end
 #   # all
 # end
+require 'pry'
 
-# def mutate_my_strings(s1,s2)
-#   s1.each_char {|c| c = s2}
-# end
+def mutate_my_strings(s1,s2)
+  all = []
+  s1.chars.map.with_index do |letter, index|
+    s1[index] = s2[index]
+    binding.pry
+    all << s1
+  end
+  binding.pry
+
+end
 
 p mutate_my_strings("right wrong", "wrong right")

4146bbf1a00fd8b286791e222580b313f06fd209
jk1dd <portajonk@gmail.com>
2017-09-18 07:39:52 -0400

    add mutate strings exercise

---
+++ mutate_strings.rb
@@ -0,0 +1,20 @@
+# I will give you two strings. I want you to transform stringOne into stringTwo one letter at a time.
+#
+# Example:
+#
+# stringOne = 'bubble gum';
+# stringTwo = 'turtle ham';
+#
+# Result:
+# bubble gum
+# tubble gum
+# turble gum
+# turtle gum
+# turtle hum
+# turtle ham
+
+# store (puts?) the intermediate strings
+
+def mutate_my_strings(s1,s2)
+  #your code here
+end

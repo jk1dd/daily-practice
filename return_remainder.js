169f6a1cefd55c21512a71b6c81823f5715b527e
jk1dd <portajonk@gmail.com>
2017-09-22 16:39:14 -0400

    add correct answer and better answer for return remainder

--- return_remainder.js
+++ return_remainder.js
@@ -5,6 +5,16 @@ function remainder(a, b){
   var array = []
   array[0] = a
   array[1] = b
-  sorted_array = array.sort()
-  return sorted_array[0] % sorted_array[1]
+  sorted_array = array.sort(function(a,b){return a-b})
+  // return sorted_array[0] % sorted_array[1]
+  return sorted_array[1] % sorted_array[0]
 } // doesn't work still
+
+console.log(remainder(17, 5))
+console.log(remainder(0, 0))
+console.log(remainder(5, 17))
+
+
+// function remainder(a, b){
+//   return a > b ? a % b : b % a;
+// }

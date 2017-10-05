4f60e377a1677a86d5a75cdbef193e3c23b26abf
jk1dd <portajonk@gmail.com>
2017-10-05 14:19:49 -0400

    small changes to js array sorter

--- sorted_arrays.js
+++ sorted_arrays.js
@@ -4,10 +4,11 @@
 //
 // nthSmallest([1,5], [2], [4,8,9], 4) // returns 5 because it's the 4th smallest value
 
-function arraySorter(...arrays, number) {
-  // const concattedArrays =
-  console.log(...arrays)
-  return 0
+function arraySorter(...arrays) {
+  const concattedArrays = arrays.reduce((acc,val)=>[...acc, ...val])
+  return concattedArrays
+  // console.log(...arrays)
+  // return 0
 }
 
 console.log(arraySorter([1,5], [2], [4,8,9], 4))

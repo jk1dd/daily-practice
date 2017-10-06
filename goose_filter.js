72dc6425aa8d08c0518418f21f905cdc417a4c6e
jk1dd <portajonk@gmail.com>
2017-10-06 15:32:20 -0400

    add goose filter js

---
+++ goose_filter.js
@@ -0,0 +1,6 @@
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((gooooooooooooooose) => !geese.includes(gooooooooooooooose))
+};
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))

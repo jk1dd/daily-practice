03ab43727ee44e9fa45cc2e9ac33b456018a3c61
jk1dd <portajonk@gmail.com>
2017-10-13 13:22:22 -0400

    add solution for two sums

--- two_sums.rb
+++ two_sums.rb
@@ -30,10 +30,45 @@
 #   # if result not found, move to next number and compare to remaining numbers (keeping track of indexes)
 # end
 
+# def two_sums(nums, target)
+#   # use combination method, found on Stack Overflow. Not quite right because it will use the same number 2x
+#   results = nums.combination(2).detect {|a,b| a + b == target}
+#   [nums.index(results[0]), nums.index(results[1])]
+# end
+
+# p two_sums([2, 7, 11, 15], 9)
+# p two_sums([2, 7, 11, 15], 18)
+# p two_sums([2, 7, 11, 15], 22)
+
+# found on leetcode
+# def two_sum(nums, target)
+#     search = Hash.new
+#     nums.each_with_index do |item,index|
+#         i = search[target-item]
+#         return [i+1, index+1] if i != nil
+#         search[item] = index
+#     end
+# end
+
+# def two_sums(nums, target)
+# 	map = {}
+# 	a = nums.each.with_index {|x, j| map[target - x] = j}
+#   require 'pry'; binding.pry
+# 	b = nums.each.with_index {|x, j| return [map[x], j] if map[x] and j != map[x]}
+# end
+
+
+
+
 def two_sums(nums, target)
-  # use combination method, found on Stack Overflow
-  results = nums.combination(2).detect {|a,b| a + b == target}
-  [nums.index(results[0]), nums.index(results[1])]
+  # nums = [2, 7, 11, 15]
+  # target = 9
+  map = {} # creates empty hash to hold differences and indexes
+  nums.each_with_index {|n, i| map[target - n] = i} # populates map with difference between each number and the index
+  # map = {7=>0, 2=>1, -2=>2, -6=>3}
+  nums.each_with_index {|n, i| return [map[n], i] if map[n] && i != map[n]} # returns the result,
+  # which is map[x] - the value where the key adds to the target, which is hte index and the index that gets it there
+  # also accounts for not using same index more than once
 end
 
 p two_sums([2, 7, 11, 15], 9)
